e28f7758e25e7f9df82b94a248f86821b81c66bc
Loskutov Vladimir <vmloskutov@gmail.com>
2019-04-19 21:06:19 +0300

    Решение задачи 1

---
+++ variables/main.js
@@ -0,0 +1,14 @@
+// Инициализация переменных
+let algebra;
+let geography;
+let physics;
+
+//Присвоение оценок
+algebra = 5;
+geography = 4;
+physics = 5;
+
+//Подсчет среднего
+let avg = (algebra + geography + physics) / 3;
+
+console.log(avg);
