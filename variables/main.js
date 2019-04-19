7799576af227463366e3a01cc8171a231cfacf47
Loskutov Vladimir <vmloskutov@gmail.com>
2019-04-19 21:23:20 +0300

    Добавлено решение 2 задачи

--- variables/main.js
+++ variables/main.js
@@ -2,13 +2,21 @@
 let algebra;
 let geography;
 let physics;
+let name;
+let message;
 
 //Присвоение оценок
 algebra = 5;
 geography = 4;
 physics = 5;
+name = "Владимир";
+
+//Составление приветственного сообщения
+message = `Привет, мир! Меня зовут ${name}.`;
 
 //Подсчет среднего
 let avg = (algebra + geography + physics) / 3;
 
-console.log(avg);
+//Вывод в консоль
+console.log(message);
+console.log("Средняя оценка по трем предметам: " + avg);
